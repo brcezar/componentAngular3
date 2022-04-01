938b374e629519a799f17a7ac26846bf9140257b
brcezar <brcsaojorge@gmail.com>
2022-03-31 21:12:34 -0300

    1 file modified

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   constructor(public httpClient: HttpClient) {}
 
-  getUsers() {
+  getUsers(page: number= 1, results: number = 5): Observable<any> {
     return this.httpClient.get('https://randomuser.me/api/?results=10');
   }
 }
